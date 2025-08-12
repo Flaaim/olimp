13b093edcc055d3e6fbeafe8c582ac679e615dc1
Flaaim <flaaim@list.ru>
2025-08-12 15:11:22 +1000

    composer remove vlucas/phpdotenv

--- config/dependencies.php
+++ config/dependencies.php
@@ -2,18 +2,6 @@
 
 declare(strict_types=1);
 
-
-use Dotenv\Dotenv;
-
-$app_env = getenv('APP_ENV');
-$file = ($app_env === 'dev') ? "dev.env" : ".env";
-if (file_exists(__DIR__ . '/common/env/' . $file)) {
-    $dotenv = Dotenv::createImmutable(__DIR__ . '/common/env/', $file);
-    $dotenv->load();
-} else {
-    exit(".Env file not found");
-}
-
 $files = glob(__DIR__ . '/common/*.php');
 
 
