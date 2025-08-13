9a82afcd82365e37abb1e0c0a65bef62d2046be6
Flaaim <flaaim@list.ru>
2025-08-13 14:21:04 +1000

    added Course tests

---
+++ src/Parser/Test/Unit/Entity/CourseTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Parser\Test\Unit\Entity;
+
+use App\Parser\Entity\Course;
+use PHPUnit\Framework\TestCase;
+
+class CourseTest extends TestCase
+{
+    public function testSuccess(): void
+    {
+        $course = new Course($id = '26020', $ticketId = '2cf27a8d06f64bdcbe15237c1053e231');
+        $this->assertEquals($id, $course->getId());
+        $this->assertEquals($ticketId, $course->getTicketId());
+    }
+
+    public function testEmpty(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        new Course('', '');
+    }
+}
