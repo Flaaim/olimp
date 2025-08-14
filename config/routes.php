5fc6dcd2da8bdec5dbafc188f7292545d90a4904
Flaaim <flaaim@list.ru>
2025-08-14 11:10:10 +1000

    added api/parse route

--- config/routes.php
+++ config/routes.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 
 use App\Http\Action\HomeAction;
+use App\Parser\ParserController;
 use Slim\App;
 
 return static function (App $app): void {
-  $app->get('/', HomeAction::class);
+    $app->get('/', HomeAction::class);
+    $app->post('/api/parse', [ParserController::class, 'parse']);
 };
