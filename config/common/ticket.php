adc497b824af1c90df33b762ce7f781768a66d1c
Flaaim <flaaim@list.ru>
2025-09-04 09:59:47 +1000

    refactored ticket repository

--- config/common/ticket.php
+++ config/common/ticket.php
@@ -2,8 +2,9 @@
 
 declare(strict_types=1);
 
+use App\Parser\Entity\Ticket\Ticket;
 use App\Ticket\Entity\TicketRepository;
-use Doctrine\ORM\EntityManager;
+
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\EntityRepository;
 use Psr\Container\ContainerInterface;
@@ -12,8 +13,8 @@ return [
     TicketRepository::class => function(ContainerInterface $container): TicketRepository {
         /** @var EntityManagerInterface $em */
         $em = $container->get(EntityManagerInterface::class);
-        
-        $repo = $em->getRepository(TicketRepository::class);
+        /** @var EntityRepository $repo */
+        $repo = $em->getRepository(Ticket::class);
         return new TicketRepository($em, $repo);
 
     }
--- src/Ticket/Entity/TicketRepository.php
+++ src/Ticket/Entity/TicketRepository.php
@@ -8,7 +8,10 @@ use Doctrine\ORM\EntityRepository;
 
 class TicketRepository
 {
-    public function __construct(private readonly EntityManagerInterface $em, private readonly EntityRepository $repo)
+    public function __construct(
+        private readonly EntityManagerInterface $em,
+        private readonly EntityRepository $repo
+    )
     {}
     public function add(Ticket $ticket): void
     {
