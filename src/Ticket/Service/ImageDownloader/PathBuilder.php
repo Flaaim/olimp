a49d1efd63c4547697f20065e7dfce225a891716
Flaaim <flaaim@list.ru>
2025-09-08 17:37:45 +1000

    removed ticketPath

--- src/Ticket/Service/ImageDownloader/PathBuilder.php
+++ src/Ticket/Service/ImageDownloader/PathBuilder.php
@@ -5,34 +5,38 @@ namespace App\Ticket\Service\ImageDownloader;
 class PathBuilder
 {
     private string $basePath;
-    private string $fullPath;
+    private string $currentPath;
     public function __construct(string $basePath)
     {
         $this->basePath = rtrim($basePath, DIRECTORY_SEPARATOR);
+        $this->currentPath = $this->basePath;
     }
     public function forTicket(string $ticketId): self
     {
-        $this->fullPath = $this->basePath . DIRECTORY_SEPARATOR . $ticketId;
+        $this->currentPath = $this->basePath . DIRECTORY_SEPARATOR . $ticketId;
         return $this;
     }
-    public function forQuestion(string $questionId, string $ticketPath): self
+    public function forQuestion(string $questionId): self
     {
-        $this->fullPath = $ticketPath . DIRECTORY_SEPARATOR . $questionId;
+        $this->currentPath = $this->currentPath . DIRECTORY_SEPARATOR . $questionId;
         return $this;
     }
     public function create(): void
     {
-        if (!is_dir($this->fullPath)) {
-            mkdir($this->fullPath, 0777, true);
+        if (!is_dir($this->currentPath)) {
+            mkdir($this->currentPath, 0777, true);
         }
     }
     public function getPath(): string
     {
-        return $this->fullPath;
+        return $this->currentPath;
     }
 
     public function getImagePath(string $filename): string
     {
-        return $this->fullPath . DIRECTORY_SEPARATOR . $filename;
+        if(empty($filename)) {
+            throw new \InvalidArgumentException('Image filename is empty');
+        }
+        return $this->currentPath . DIRECTORY_SEPARATOR . trim($filename, DIRECTORY_SEPARATOR);
     }
 }
--- src/Ticket/Test/Unit/Service/PathBuilderTest.php
+++ src/Ticket/Test/Unit/Service/PathBuilderTest.php
@@ -14,4 +14,19 @@ class PathBuilderTest extends TestCase
 
         $this->assertEquals('/tmp/1234', $builder->getPath());
     }
+
+    public function testGetPath(): void
+    {
+        $builder = (new PathBuilder(sys_get_temp_dir()))->forTicket('1234');
+
+        $this->assertEquals('/tmp/1234', $builder->getPath());
+        $this->assertEquals('/tmp/1234/image.jpg', $builder->getImagePath('image.jpg'));
+        $this->assertEquals('/tmp/1234/image.jpg', $builder->getImagePath('/image.jpg'));
+    }
+    public function testGetImagePathEmpty(): void
+    {
+        $builder = (new PathBuilder(sys_get_temp_dir()))->forTicket('1234');
+        $this->expectException(\InvalidArgumentException::class);
+        $builder->getImagePath('');
+    }
 }
