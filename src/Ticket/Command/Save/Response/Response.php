582ffa200523282221e364a1de436b8230703087
Flaaim <flaaim@list.ru>
2025-09-09 13:53:27 +1000

    added questions array

--- src/Ticket/Command/Save/Response/Response.php
+++ src/Ticket/Command/Save/Response/Response.php
@@ -2,21 +2,42 @@
 
 namespace App\Ticket\Command\Save\Response;
 
+use App\Parser\Entity\Ticket\Ticket;
 use Webmozart\Assert\Assert;
 
 class Response implements \JsonSerializable
 {
     public function __construct(
-        private string $id,
-        private string $name,
-        private string $cipher
+        private readonly string  $id,
+        private readonly ?string $name,
+        private readonly ?string $cipher,
+        private readonly array $questions,
     ){}
+
+    public static function fromResult(Ticket $ticket): self
+    {
+
+        return new self(
+            $ticket->getId(),
+            $ticket->getName(),
+            $ticket->getCipher(),
+            $ticket->getQuestions()->toArray(),
+        );
+    }
     public function jsonSerialize(): array
     {
         return [
             'id' => $this->id,
             'name' => $this->name,
             'cipher' => $this->cipher,
+            'questions' => array_map(
+                fn ($question) => [
+                    'id' => $question->getId(),
+                    'number' => $question->getNumber(),
+                    'text' => $question->getText(),
+                    'image' => $question->getQuestionMainImg(),
+                ], $this->questions
+            )
         ];
     }
 }
