24c31f8697c64b57215fc392a2efd57824a9a388
Flaaim <flaaim@list.ru>
2025-09-30 16:44:44 +1000

    added EmailType.php

---
+++ src/Permit/Entity/EmailType.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Permit\Entity;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\StringType;
+
+class EmailType extends StringType
+{
+    const NAME = 'email';
+
+    public function convertToDatabaseValue($value, AbstractPlatform $platform): mixed
+    {
+        return $value instanceof Email ? $value->getValue() : $value;
+    }
+
+    public function convertToPHPValue($value, AbstractPlatform $platform): ?Email
+    {
+        return !empty($value) ? new Email((string)$value) : null;
+    }
+    public function getName(): string
+    {
+        return self::NAME;
+    }
+}
