82d70470c056446e6b873723cff08dd9bfec0e00
Flaaim <flaaim@list.ru>
2025-10-04 08:38:26 +1000

    updated YookassaConfig

--- config/common/payment.php
+++ config/common/payment.php
@@ -3,9 +3,9 @@
 declare(strict_types=1);
 
 
+use App\Shared\Domain\Service\Payment\Provider\YookassaConfig;
 use App\Shared\Domain\Service\Payment\Provider\YookassaProvider;
 use Psr\Container\ContainerInterface;
-use Psr\Log\LoggerInterface;
 use YooKassa\Client;
 
 return [
@@ -14,13 +14,20 @@ return [
 
         $client = new Client();
 
-        $client->setLogger($container->get(LoggerInterface::class));
+        $yookassaConfig = new YookassaConfig(
+            $config['name'],
+            $config['shopId'],
+            $config['secretKey'],
+            $config['returnUrl']
+        );
+
         $client->setAuth($config['shopId'], $config['secretKey']);
 
-        return new YookassaProvider($client, $config['return_url']);
+        return new YookassaProvider($client, $yookassaConfig);
   },
     'config' => [
         'payment' => [
+            'name' => 'Yookassa',
             'shopId' => '221345',
             'secretKey' => 'test_0B3flJqsbdKNA2sS2dT0ahs74LtF7fwJq2oVR-8wTCM',
             'returnUrl' => 'http://localhost:8080/payment/success',
