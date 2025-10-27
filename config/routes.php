0b8a2e90b53955e2e9123d3aca9f34be5d7d6518
Flaaim <flaaim@list.ru>
2025-10-27 19:18:30 +1000

    added Parse RequestAction

--- config/routes.php
+++ config/routes.php
@@ -2,14 +2,21 @@
 
 declare(strict_types=1);
 
-use App\Parser\ParserController;
+
+use App\Http\Action\HomeAction;
 use App\Ticket\TicketController;
 use Slim\App;
+use Slim\Routing\RouteCollectorProxy;
+use App\Http\Action\Parser\Parse;
 
 return static function (App $app): void {
-    $app->get('/', \App\Http\Action\HomeAction::class);
+    $app->get('/', HomeAction::class);
+    $app->group('/api', function (RouteCollectorProxy $group) {
+        $group->group('/parser', function (RouteCollectorProxy $group) {
+            $group->post('/parse', Parse\RequestAction::class);
 
-    $app->post('/api/parser/parse', [ParserController::class, 'parse']);
+        });
+    });
 
     $app->post('/api/ticket/add', [TicketController::class, 'add']);
 
---
+++ src/Http/Action/Parser/Parse/RequestAction.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Action\Parser\Parse;
+
+use App\Http\JsonResponse;
+use App\Parser\Command\ParserCommand;
+use App\Parser\Command\ParserHandler;
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use RuntimeException;
+
+class RequestAction implements RequestHandlerInterface
+{
+    public function __construct(private readonly ContainerInterface $container)
+    {}
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $data = $request->getParsedBody() ?? [];
+        try {
+            if(empty($data)) {
+                throw new RuntimeException('Request data is empty');
+            }
+            $command = new ParserCommand(
+                $data['host'] ?? '',
+                $data['branchId'] ?? '',
+                $data['ticketId'] ?? '',
+                $data['cookie'] ?? '',
+                $data['options'] ?? []
+            );
+            $handler = $this->container->get(ParserHandler::class);
+            /** @var ParserHandler $handler */
+            $response = $handler->handle($command);
+            return new JsonResponse($response);
+        }catch (\Exception $e){
+            return new JsonResponse(['message' => $e->getMessage()], 500);
+        }
+    }
+}
