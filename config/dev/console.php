d69b681500b0d17b1b62d7ac0a5d1c760c5fc1b0
Flaaim <flaaim@list.ru>
2025-11-08 18:06:58 +1000

    Added fixtures load command

--- config/dev/console.php
+++ config/dev/console.php
@@ -2,20 +2,37 @@
 
 declare(strict_types=1);
 
+use App\Console\FixturesLoadCommand;
 use Doctrine\Migrations\Tools\Console\Command\DiffCommand;
 use Doctrine\Migrations\Tools\Console\Command\GenerateCommand;
-use Doctrine\ORM\Tools\Console\Command\SchemaTool\CreateCommand;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Tools\Console\Command\SchemaTool\DropCommand;
+use Psr\Container\ContainerInterface;
 
 return [
+    FixturesLoadCommand::class => function (ContainerInterface $container) {
+        $config = $container->get('config')['console'];
+
+        $em = $container->get(EntityManagerInterface::class);
+
+        return new FixturesLoadCommand(
+          $em,
+          $config['fixture_paths'],
+        );
+    },
     'config' => [
         'console' => [
             'commands' => [
+                FixturesLoadCommand::class,
+
                 DropCommand::class,
 
                 DiffCommand::class,
                 GenerateCommand::class,
-            ]
-        ]
+            ],
+            'fixture_paths' => [
+                __DIR__ . '/../../src/Parser/Fixture',
+            ],
+        ],
     ]
 ];
